378d0009f4cf39c9a0488fcf8b43b27b3bf2bd40
Broe <daniel.broe@bs.nttdata.com>
2021-11-08 15:24:03 +0100

    pagination and account pages template

--- UI/pages/events/index.tsx
+++ UI/pages/events/index.tsx
@@ -1,6 +1,8 @@
 import EventItem from '@/components/EventItem';
 import Layout from '@/components/layout';
+import Pagination from '@/components/Pagination';
 import { API_URL } from '../../config/index';
+const PER_PAGE = 5;
 
 type JSONValue = {
   id: string;
@@ -17,9 +19,12 @@ type JSONValue = {
 
 type pageProps = {
   events: JSONValue[];
+  page: number;
+  total: number;
 };
 
-export default function EventsPage({ events }: pageProps) {
+export default function EventsPage({ events, page, total }: pageProps) {
+  const lastPage = Math.ceil(total / PER_PAGE);
   return (
     <Layout>
       <h1>Events</h1>
@@ -28,18 +33,30 @@ export default function EventsPage({ events }: pageProps) {
       {events?.map((evt) => (
         <EventItem key={evt.id} evt={evt} />
       ))}
+
+      <Pagination page={page} total={total} perPage={PER_PAGE} />
     </Layout>
   );
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/events?_sort=date:ASC`);
-  const events = await res.json();
+export async function getServerSideProps({ query: { page = 1 } }) {
+  // Calculate startpage
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE;
+  // Fetch events
+  const eventRes = await fetch(
+    `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
+  );
+  const events = await eventRes.json();
+
+  const totalRes = await fetch(`${API_URL}/events/count`);
+
+  const total = await totalRes.json();
 
   return {
     props: {
       events,
-      revalidate: 1,
+      page: +page,
+      total,
     },
   };
 }
