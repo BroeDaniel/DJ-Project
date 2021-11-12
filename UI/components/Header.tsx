d0640b2cac24a554db7706194e2cf1a8b105ecff
Broe <daniel.broe@bs.nttdata.com>
2021-11-12 12:40:42 +0100

    register and login form

--- UI/components/Header.tsx
+++ UI/components/Header.tsx
@@ -1,3 +1,4 @@
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 import Link from 'next/link';
 import styles from '@/styles/Header.module.css';
 import Search from './Search';
@@ -16,6 +17,14 @@ export default function Header() {
           </li>
           <li>
             <Link href='/events/add'>Add event</Link>
+          </li>
+          <li>
+            <Link href='/account/login'>
+              <a className='btn-secondary btn-icon'>
+                <FaSignInAlt />
+                Login
+              </a>
+            </Link>
           </li>
         </ul>
       </nav>
